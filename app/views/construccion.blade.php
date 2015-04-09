ae625354f63aa1d934b561e4b87a4ccd79813f16
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-08 20:06:03 -0500

    vista de construccion

---
+++ app/views/construccion.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Portal Perú</title>
+<style>
+    div{
+        width: 1000px;
+        margin: 0 auto;
+    }
+</style>
+</head>
+<body>
+    <div>
+        <img src="/imagenes/construccion.jpg" alt="Portal Perú"/>
+    </div>
+</body>
+</html>
