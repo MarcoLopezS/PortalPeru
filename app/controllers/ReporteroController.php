09e432ca1d7abe8f16761d4fbd09f2a9b8c21448
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-15 00:38:12 -0500

    verificacion de codigo de usuario

--- app/controllers/ReporteroController.php
+++ app/controllers/ReporteroController.php
@@ -37,21 +37,68 @@ class ReporteroController extends BaseController {
         {
             $user = new User($dataUser);
             $user->type = 'reportero';
+            $user->activacion = 1;
             $user->save();
 
             $emailUser = User::whereEmail(Input::get('email'))->first();
+            $actCodigo = CodigoAleatorio(25,true, true, false);
 
             $userProfile = new UserProfile($dataProfile);
             $userProfile->user_id = $emailUser->id;
+            $userProfile->activacion_codigo = $actCodigo;
             $userProfile->save();
 
+            /*
+            $datosCorreo = [
+                'nombre' => Input::get('nombre'),
+                'apellidos' => Input::get('apellidos'),
+                'codigo' => $actCodigo
+            ];
+
+            $fromEmail = 'no-reply@portalperu.pe';
+            $fromNombre = 'Portal Perú';
+
+            Mail::send('emails.reportero-register', $datosCorreo, function($message) use ($fromNombre, $fromEmail){
+                $message->to(Input::get('email'), Input::get('nombre'));
+                $message->from($fromEmail, $fromNombre);
+                $message->subject('Portal Perú - Activar cuenta');
+            });
+
+            */
+
+            $message = 'verify';
+
             //REDIRECCIONAR A PAGINA PARA VER DATOS
-            return View::make('reportero.verify');
+            return View::make('reportero.verify', compact('message'));
         }
         else
         {
             return Redirect::back()->with('errors', array_merge_recursive($validatorUser->messages()->toArray(), $validatorProfile->messages()->toArray()))->withInput();
         }
+    }
+
+    public function verifyView()
+    {
+        return View::make('reportero.verify');
+    }
+
+    public function verify($codigo)
+    {
+        $userVerify = UserProfile::whereActivacionCodigo($codigo)->first();
+        $user = User::whereId($userVerify->user_id)->first();
+
+        if($userVerify->activacion == 0)
+        {
+            $user->activacion = 1;
+            $user->save();
+            $message = 'verify-active';
+        }
+        elseif($userVerify->activacion == 1)
+        {
+            $message = 'verify-noactive';
+        }
+
+        return View::make('reportero.verify', compact('message'));
     }
 
     public function loginView()
