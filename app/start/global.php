c86df4d22259f3525cc6045e41c57ba35166fc74
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-15 00:39:13 -0500

    funciones para eliminar textos y generar url amigable, agregando fecha a upload de imagen

--- app/start/global.php
+++ app/start/global.php
@@ -112,10 +112,52 @@ function FileMove($file, $path)
     $name = $file->getClientOriginalName();					//NOMBRE Y EXTENSION DE ARCHIVO
     $extension = $file->getClientOriginalExtension();		//EXTENSION DE ARCHIVO
     $archive = basename($name, ".".$extension);			    //NOMBRE DE ARCHIVO
-    $pathName = Str::slug($archive);						//CONVERTIR NOMBRE SIN ESPACIOS
+    $pathName = SlugUrl($archive)."-".date('YmdHi');        //CONVERTIR NOMBRE SIN ESPACIOS
     $filename = $pathName.".".$extension;					//NOMBRE Y EXTENSION SIN ESPACIOS
     $file->move($path, $filename);							//MOVER ARCHIVO A NUEVA CARPETA
     return $filename;
+}
+
+function SlugUrl($texto){
+    return getUrlAmigable(eliminarTextoURL($texto));
+}
+
+function fechaActual(){
+    $fechaActual = date('Y-m-d H:i:s');
+    return $fechaActual;
+}
+
+function eliminarTextoURL($str) {
+    $search = array('&lt;', '&gt;', '&quot;', '&amp;');
+    $str = str_replace($search, '', $str);
+    $search = array('&aacute;','&Aacute;','&eacute;','&Eacute;','&iacute;','&Iacute;','&oacute;','&Oacute;','&uacute;','&Uacute;','&ntilde;','&Ntilde;');
+    $replace = array('a','a','e','e','i','i','o','o','u','u','n','n');
+    $search = array('Á', 'É', 'Í', 'Ó', 'Ú', 'á', 'é', 'í', 'ó', 'ú', 'Ü', 'ü', 'Ñ', 'ñ', '_', '-');
+    $replace = array('a', 'e', 'i', 'o', 'u', 'a', 'e', 'i', 'o', 'u', 'u', 'u', 'n', 'n', ' ', ' ');
+    $str = str_replace($search, $replace, $str);
+    $str = preg_replace('/&(?!#[0-9]+;)/s', '', $str);
+    $search = array(' a ', ' de ', ' con ', ' por ', ' en ', ' y ', ' e ', ' o ', ' u ', ' la ', ' el ', ' lo ', ' las ', ' los ', ' fue ', ' del ', ' se ', ' su ', ' una ');
+    $str = str_replace($search, ' ', strtolower($str));
+    $str = str_replace($search, $replace, strtolower(trim($str)));
+    $str = preg_replace("/[^a-zA-Z0-9\s]/", '', $str);
+    $str = preg_replace('/\s\s+/', ' ', $str);
+    $str = str_replace(' ', '-', $str);
+    return  $str;
+}
+
+function getUrlAmigable($s){
+    $s = strtolower($s);
+    $s = preg_replace("[áàâãäª@]","a",$s);
+    $s = preg_replace("[éèêë]","e",$s);
+    $s = preg_replace("[íìîï]","i",$s);
+    $s = preg_replace("[óòôõºö]","o",$s);
+    $s = preg_replace("[úùûü]","u",$s);
+    $s = preg_replace("[ç]","c",$s);
+    $s = preg_replace("[ñ]","n",$s);
+    $s = preg_replace( "/[^a-zA-Z0-9\-]/", "-", $s );
+    $s = preg_replace( array("`[^a-z0-9]`i","`[-]+`") , "-", $s);
+
+    return trim($s, '-');
 }
 
 //CARPETA CON NOMBRE DEL MES ACTUAL
