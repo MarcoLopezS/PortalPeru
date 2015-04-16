6df01c140907ad411bc8686c9e1382769ef18ce9
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-16 12:32:42 -0500

    contador de notas no publicadas de reportero ciudadana y contador de social

--- app/start/global.php
+++ app/start/global.php
@@ -12,6 +12,7 @@
 */
 
 use PortalPeru\Entities\Configuration;
+use PortalPeru\Entities\Post;
 
 ClassLoader::addDirectories(array(
 
@@ -104,6 +105,45 @@ function configWeb()
 {
     $baseUrl = Configuration::find(1);
     return $baseUrl;
+}
+
+function reporteroCountNoPuplic()
+{
+    $count = Post::whereCategoryId(6)->wherePublicar(0)->count();
+    return $count;
+}
+
+//COUNTS SOCIAL
+function get_tweets($url) {
+
+    $json_string = file_get_contents('http://urls.api.twitter.com/1/urls/count.json?url=' . $url);
+    $json = json_decode($json_string, true);
+
+    return intval( $json['count'] );
+}
+
+function get_likes($url) {
+
+    $json_string = file_get_contents('http://graph.facebook.com/?ids=' . $url);
+    $json = json_decode($json_string, true);
+
+    return intval( $json[$url]['likes'] );
+}
+
+function get_plusones($url) {
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    $curl_results = curl_exec ($curl);
+    curl_close ($curl);
+
+    $json = json_decode($curl_results, true);
+
+    return intval( $json[0]['result']['metadata']['globalCounts']['count'] );
 }
 
 //MOVER ARCHIVO
