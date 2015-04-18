b62037093dba6036392b24f839a7f53cd761403a
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-18 11:03:01 -0500

    calidad de imagen

--- app/controllers/ImageController.php
+++ app/controllers/ImageController.php
@@ -5,7 +5,7 @@ class ImageController extends BaseController {
     public function adaptiveResize($folder, $width, $height, $image)
     {
         $file = 'upload/' . $folder . '/' .$image;
-        $thumb = PhpThumbFactory::create($file);
+        $thumb = PhpThumbFactory::create($file, ['jpegQuality' => 80]);
         $thumb->adaptiveResize($width, $height);
         $thumb->show();
     }
