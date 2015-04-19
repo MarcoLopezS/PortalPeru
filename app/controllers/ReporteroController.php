3b49a0459c9a66c9abea08eeedde952677dd5a9d
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-19 00:33:07 -0500

    funciones para envio de mensaje de activacion

--- app/controllers/ReporteroController.php
+++ app/controllers/ReporteroController.php
@@ -37,18 +37,17 @@ class ReporteroController extends BaseController {
         {
             $user = new User($dataUser);
             $user->type = 'reportero';
-            $user->activacion = 1;
+            $user->activacion = 0;
             $user->save();
 
             $emailUser = User::whereEmail(Input::get('email'))->first();
-            $actCodigo = CodigoAleatorio(25,true, true, false);
+            $actCodigo = CodigoAleatorio(50,true, true, false);
 
             $userProfile = new UserProfile($dataProfile);
             $userProfile->user_id = $emailUser->id;
             $userProfile->activacion_codigo = $actCodigo;
             $userProfile->save();
 
-            /*
             $datosCorreo = [
                 'nombre' => Input::get('nombre'),
                 'apellidos' => Input::get('apellidos'),
@@ -58,13 +57,11 @@ class ReporteroController extends BaseController {
             $fromEmail = 'no-reply@portalperu.pe';
             $fromNombre = 'Portal Perú';
 
-            Mail::send('emails.reportero-register', $datosCorreo, function($message) use ($fromNombre, $fromEmail){
+            Mail::queue('emails.reportero-register', $datosCorreo, function($message) use ($fromNombre, $fromEmail){
                 $message->to(Input::get('email'), Input::get('nombre'));
                 $message->from($fromEmail, $fromNombre);
                 $message->subject('Portal Perú - Activar cuenta');
             });
-
-            */
 
             $message = 'verify';
 
@@ -74,6 +71,60 @@ class ReporteroController extends BaseController {
         else
         {
             return Redirect::back()->with('errors', array_merge_recursive($validatorUser->messages()->toArray(), $validatorProfile->messages()->toArray()))->withInput();
+        }
+    }
+
+    public function correoActivarCuenta()
+    {
+        return View::make('reportero.sendemail-active');
+    }
+
+    public function correoActivarCuentaForm()
+    {
+        $rules = [
+            'email' => 'required|email'
+        ];
+
+        $data = Input::only('email');
+
+        $validatorUser = Validator::make($data, $rules);
+
+        if($validatorUser->passes())
+        {
+            $user = User::whereEmail(Input::get('email'))->first();
+
+            if(!$user)
+            {
+                return Redirect::back()->with('login_error', "El email no está registrado.")->withInput();
+            }
+            else
+            {
+                $userProfile = UserProfile::whereUserId($user->id)->first();
+
+                $datosCorreo = [
+                    'nombre' => $userProfile->nombre,
+                    'apellidos' => $userProfile->apellidos,
+                    'codigo' => $userProfile->activacion_codigo
+                ];
+
+                $fromEmail = 'no-reply@portalperu.pe';
+                $fromNombre = 'Portal Perú';
+
+                Mail::queue('emails.reportero-register', $datosCorreo, function($message) use ($fromNombre, $fromEmail){
+                    $message->to(Input::get('email'), Input::get('nombre'));
+                    $message->from($fromEmail, $fromNombre);
+                    $message->subject('Portal Perú - Activar cuenta');
+                });
+
+                $message = 'verify';
+
+                //REDIRECCIONAR A PAGINA PARA VER DATOS
+                return View::make('reportero.verify', compact('message'));
+            }            
+        }
+        else
+        {
+            return Redirect::back()->with('errors', $validatorUser->messages())->withInput();
         }
     }
 
