0d0d93d40f0123ce28651d11c7fd3299001474f9
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-22 00:38:58 -0500

    agregando funciones para relacionarlo con la entidad PostHistory y agregando SoftDelete

--- app/PortalPeru/Entities/Post.php
+++ app/PortalPeru/Entities/Post.php
@@ -1,6 +1,12 @@
 <?php namespace PortalPeru\Entities;
 
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
 class Post extends BaseEntity{
+
+    use SoftDeletingTrait;
+
+    protected $dates = ['deleted_at'];
 
     protected $fillable = ['titulo','slug_url','descripcion','contenido','imagen','imagen_carpeta','redaccion','publicar','category_id','post_order_id','published_at','user_id'];
 
@@ -22,6 +28,16 @@ class Post extends BaseEntity{
     public function postPhoto()
     {
         return $this->hasMany('PortalPeru\Entities\PostPhoto');
+    }
+
+    public function postHistory()
+    {
+        return $this->hasMany('PortalPeru\Entities\PostHistory');
+    }
+
+    public function postUserDelete()
+    {
+        return $this->hasMany('PortalPeru\Entities\PostHistory')->whereType('delete')->orderBy('created_at', 'desc')->first();
     }
 
 } 
