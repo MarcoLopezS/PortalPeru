ef40581a2c61563e9362553f630c83a3841eff0c
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-22 00:40:29 -0500

    funcion para buscar en post eliminados

--- app/PortalPeru/Repositories/PostRepo.php
+++ app/PortalPeru/Repositories/PostRepo.php
@@ -15,4 +15,28 @@ class PostRepo extends BaseRepo{
     {
         $q->where('category_id', $value);
     }
+
+    //FILTRAR
+    public function searchDeletes(array $data = array(), $paginate = false, $orderField, $orderType)
+    {
+        $data = array_only($data, $this->filters);
+        $data = array_filter($data, 'strlen');
+        $q = $this->getModel()->onlyTrashed()->select()->orderBy($orderField, $orderType);
+        foreach ($data as $field => $value)
+        {
+            // slug_url -> filterBySlugUrl
+            $filterMethod = 'filterBy' . studly_case($field);
+            if (method_exists(get_called_class(), $filterMethod))
+            {
+                $this->$filterMethod($q, $value);
+            }
+            else
+            {
+                $q->where($field, $data[$field]);
+            }
+        }
+        return $paginate ?
+            $q->paginate()->appends($data)
+            : $q->get();
+    }
 }
