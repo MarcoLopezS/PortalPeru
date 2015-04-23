386d51e5c9473adafc0657bc0b8c073871b24fad
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-23 16:11:12 -0500

    agregando funcion para busqueda de reporteros

--- app/PortalPeru/Repositories/UserRepo.php
+++ app/PortalPeru/Repositories/UserRepo.php
@@ -15,4 +15,28 @@ class UserRepo extends BaseRepo{
     {
         $q->where('email', 'LIKE', "%$value%");
     }
+
+    //FILTRAR
+    public function searchReportero(array $data = array(), $paginate = false, $orderField, $orderType)
+    {
+        $data = array_only($data, $this->filters);
+        $data = array_filter($data, 'strlen');
+        $q = $this->getModel()->select()->whereType('reportero')->orderBy($orderField, $orderType);
+        foreach ($data as $field => $value)
+        {
+            // slug_url -> filterBySlugUrl
+            $filterMethod = 'filterBy' . studly_case($field);
+            if (method_exists(get_called_class(), $filterMethod))
+            {
+                $this->$filterMethod($q, $value);
+            }
+            else
+            {
+                $q->where($field, $data[$field]);
+            }
+        }
+        return $paginate ?
+            $q->paginate()->appends($data)
+            : $q->get();
+    }
 }
