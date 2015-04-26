8690aafae4647032dafff6594a033cf156791a30
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-26 11:59:21 -0500

    funcion de busqueda de usuarios tipo Admin y Editor

--- app/PortalPeru/Repositories/UserRepo.php
+++ app/PortalPeru/Repositories/UserRepo.php
@@ -39,4 +39,28 @@ class UserRepo extends BaseRepo{
             $q->paginate()->appends($data)
             : $q->get();
     }
+
+    //FILTRAR
+    public function searchUsers(array $data = array(), $paginate = false, $orderField, $orderType)
+    {
+        $data = array_only($data, $this->filters);
+        $data = array_filter($data, 'strlen');
+        $q = $this->getModel()->select()->whereType('admin')->orWhere('type','editor')->orderBy($orderField, $orderType);
+        foreach ($data as $field => $value)
+        {
+            // slug_url -> filterBySlugUrl
+            $filterMethod = 'filterBy' . studly_case($field);
+            if (method_exists(get_called_class(), $filterMethod))
+            {
+                $this->$filterMethod($q, $value);
+            }
+            else
+            {
+                $q->where($field, $data[$field]);
+            }
+        }
+        return $paginate ?
+            $q->paginate()->appends($data)
+            : $q->get();
+    }
 }
