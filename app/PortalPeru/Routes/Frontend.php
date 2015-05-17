4ce65fbe5b715885530fb6137885cbed826d1ecd
Marco Lopez <mlopez@marostdevelopers.com>
2015-05-17 17:19:22 -0500

    separacion de rutas

--- app/PortalPeru/Routes/Frontend.php
+++ app/PortalPeru/Routes/Frontend.php
@@ -2,13 +2,22 @@
 
 /* FRONTEND */
 Route::get('/', ['as' => 'home', 'uses' => 'FrontendController@home']);
+
+/* CONTACTO */
 Route::get('nosotros', ['as' => 'home.nosotros', 'uses' => 'FrontendController@nosotros']);
 Route::get('contacto', ['as' => 'home.contacto', 'uses' => 'FrontendController@contacto']);
 Route::get('publicidad', ['as' => 'home.publicidad', 'uses' => 'FrontendController@publicidad']);
+
+/* NOTICIAS */
 Route::get('nota/{id}-{url}', ['as' => 'home.noticia', 'uses' => 'FrontendController@noticia']);
 Route::get('seccion/{url}', ['as' => 'home.noticia.categoria', 'uses' => 'FrontendController@noticiaCategoria']);
 Route::get('tag/{id}-{url}', ['as' => 'home.noticia.tags', 'uses' => 'FrontendController@noticiaTags']);
+Route::get('mira-peru', ['as' => 'home.noticia.miraperu', 'uses' => 'FrontendController@noticiaMiraPeru']);
+
+/* BUSCADOR */
 Route::get('buscar/b={texto}', ['as' => 'home.buscar', 'uses' => 'FrontendController@buscar']);
+
+/* COLUMNISTAS */
 Route::get('columnistas', ['as' => 'home.columnistas.list', 'uses' => 'FrontendController@columnistasList']);
 Route::get('columnistas/{id}-{url}', ['as' => 'home.columnistas.person', 'uses' => 'FrontendController@columnistasPerson']);
 Route::get('columnistas/{id}-{url}/{idColumn}-{urlColumn}', ['as' => 'home.columnistas.column', 'uses' => 'FrontendController@columnistasColumn']);
