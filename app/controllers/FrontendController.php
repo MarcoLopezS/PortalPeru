9d1c572bce9d86c32ead768b3b92dc4f8a61b893
Marco Lopez <mlopez@marostdevelopers.com>
2015-05-17 17:37:10 -0500

    mostrar las 8 primeras noticias de mira el peru

--- app/controllers/FrontendController.php
+++ app/controllers/FrontendController.php
@@ -230,4 +230,21 @@ class FrontendController extends BaseController{
         return View::make('frontend.columnista-noticia', compact('columnista','columna', 'columnistasDia'));
     }
 
+    public function noticiaMiraPeru()
+    {
+        //NOTICIAS
+        $mira_peru = Post::where('category_id', 5)->where('publicar', 1)->orderBy('published_at','desc')->paginate(8);
+
+        //COLUNISTAS DEL DIA
+        if(date('N')==1){ $columnistasDia = Columnist::whereDiaLunes(1)->orderBy('orden', 'asc')->wherePublicar(1)->get(); }
+        elseif(date('N')==2){ $columnistasDia = Columnist::whereDiaMartes(1)->orderBy('orden', 'asc')->wherePublicar(1)->get(); }
+        elseif(date('N')==3){ $columnistasDia = Columnist::whereDiaMiercoles(1)->orderBy('orden', 'asc')->wherePublicar(1)->get(); }
+        elseif(date('N')==4){ $columnistasDia = Columnist::whereDiaJueves(1)->orderBy('orden', 'asc')->wherePublicar(1)->get(); }
+        elseif(date('N')==5){ $columnistasDia = Columnist::whereDiaViernes(1)->orderBy('orden', 'asc')->wherePublicar(1)->get(); }
+        elseif(date('N')==6){ $columnistasDia = Columnist::whereDiaSabado(1)->orderBy('orden', 'asc')->wherePublicar(1)->get(); }
+        elseif(date('N')==7){ $columnistasDia = Columnist::whereDiaDomingo(1)->orderBy('orden', 'asc')->wherePublicar(1)->get(); }
+
+        return View::make('frontend.miraperu', compact('mira_peru', 'columnistasDia'));
+    }
+
 }
