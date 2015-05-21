325e4b748264065f7a95d0bc35e5a61cc77bd235
Marco Lopez <mlopez@marostdevelopers.com>
2015-05-21 00:23:55 -0500

    cambio de orden al llamar route

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,6 @@
 /* IMAGENES */
 Route::get('/upload/{folder}/{width}x{height}/{image}', ['as' => 'image.adaptiveResize', 'uses' => 'ImageController@adaptiveResize']);
 
-include_once('PortalPeru/Routes/Frontend.php');
-
 include_once('PortalPeru/Routes/Administrador.php');
+
+include_once('PortalPeru/Routes/Frontend.php');
