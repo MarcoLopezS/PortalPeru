d7ed75b8de22b988504cbd779176a0f49aebd090
Marco Lopez <mlopez@marostdevelopers.com>
2016-05-06 19:26:42 -0500

    eliminacion de rules required en categorua

--- app/controllers/AdminPostsController.php
+++ app/controllers/AdminPostsController.php
@@ -17,7 +17,7 @@ class AdminPostsController extends \BaseController {
         'descripcion' => 'required|min:10|max:255',
         'contenido' => 'required',
         'imagen' => 'mimes:jpeg,jpg,png',
-        'categoria' => 'required',
+        'categoria' => '',
         'published_at' => 'required',
         'publicar' => 'required|in:1,0'
     ];
